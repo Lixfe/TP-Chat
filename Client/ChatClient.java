21d8773148c96f9713ccb7466fe7d0492b7b1dbe
RTRaineau <raphael.traineau@gmail.com>
2013-10-21 10:54:51 +0200

    afficher de chatclient + test dans main

--- Client/ChatClient.java
+++ Client/ChatClient.java
@@ -18,7 +18,7 @@ public class ChatClient {
   
   //METHODES
   public void afficher(LinkedList<Message> msglist ) {
-	  for (int i = msglist.size(); i >= 0 ; i--) {
+	  for (int  i = 0 ; i < msglist.size(); i++) {
 		System.out.println(msglist.get(i).getContenu());
 	}
   }
