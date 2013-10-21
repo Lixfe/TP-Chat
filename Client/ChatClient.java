765624c56a9e2be560f8ef554f8bad6f0c027241
RTRaineau <raphael.traineau@gmail.com>
2013-10-21 10:46:52 +0200

    chatclient afficher

--- Client/ChatClient.java
+++ Client/ChatClient.java
@@ -1,21 +1,26 @@
 package Client;
 import java.util.LinkedList;
 
-import Partage.Message;
+import Partage.*;
 
 public class ChatClient {
 
+  //ATTRIBUTS
   public Integer numDernierMessage;
-
   protected String adresseServeur;
 
+  //CONSTRUCTEUR
+  public ChatClient() {
+	  this.adresseServeur="";
+	  this.numDernierMessage = 0;
+  }
+
+  
+  //METHODES
   public void afficher(LinkedList<Message> msglist ) {
 	  for (int i = msglist.size(); i >= 0 ; i--) {
 		System.out.println(msglist.get(i).getContenu());
 	}
-  }
-
-  public void ChatClient() {
   }
 
 }
