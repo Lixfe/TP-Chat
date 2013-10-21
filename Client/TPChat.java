87a0f8d6b462de4ef959b5acb2a0d0f555b946a2
RTRaineau <raphael.traineau@gmail.com>
2013-10-21 11:27:02 +0200

    main client

--- Client/TPChat.java
+++ Client/TPChat.java
@@ -1,7 +1,12 @@
 package Client;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.LinkedList;
 
 import Partage.*;
+import Serveur.* ;
 
 public class TPChat {
 
@@ -11,7 +16,24 @@ public class TPChat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		String url = args[0]; // premier parametre, indiquant l'url du serveur distant
+		
+		try {
+			ChatServeur serveur = (ChatServeur) Naming.lookup(url);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 	}
 
 }
