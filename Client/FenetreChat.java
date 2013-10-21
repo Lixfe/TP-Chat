9f9b8b19cafaa0094fa53f62b546852da4c56664
Lixfe <felix.leo.lecuyer@gmail.com>
2013-10-21 21:59:38 +0200

    Creation classes d'affichage graphique

---
+++ Client/FenetreChat.java
@@ -0,0 +1,46 @@
+package Client;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Classe FenetreChat : cette classe permet l'affichage de l'environnement de chat.
+ * Cet environnement contient un champ d'affichage des messages et un champ de saisie de texte.
+ * 
+ * @author felix
+ *
+ */
+public class FenetreChat {
+	
+    private JFrame fenetre; 
+    private JLabel affichage; 
+    private JTextField champSaisie; 
+    private JButton boutonEnvoyer;
+    private JButton boutonQuitter;
+    
+    /**
+     * Constructeur FenetreChat par defaut : initialise les objets d'affichage.
+     */
+	public FenetreChat(){
+		this.fenetre = new JFrame("Fenetre de chat");
+		this.affichage = new JLabel();
+		this.champSaisie = new JTextField(100);
+		this.boutonEnvoyer = new JButton("Envoyer");
+		this.boutonQuitter = new JButton("Quitter");
+		
+		Container conteneur = fenetre.getContentPane(); 
+	    FlowLayout disposition = new FlowLayout(); 
+	    
+	    conteneur.setLayout(disposition); 
+	    conteneur.add(this.affichage); 
+	    conteneur.add(this.champSaisie); 
+	    conteneur.add(this.boutonEnvoyer);
+	    conteneur.add(this.boutonQuitter);
+	    
+	    this.fenetre.pack(); 
+	    this.fenetre.setVisible(true); 
+	}
+	
+}
+
+
