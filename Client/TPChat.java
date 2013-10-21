fce8268ecc30bd34f0978918344aeb63c99a962d
RTRaineau <raphael.traineau@gmail.com>
2013-10-21 10:58:41 +0200

    main client

---
+++ Client/TPChat.java
@@ -0,0 +1,17 @@
+package Client;
+import java.util.LinkedList;
+
+import Partage.*;
+
+public class TPChat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
